028452ff66e0cb81444603ec2e476c2ebf142918
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-05 16:53:27 -0300

    feat: sum numbers

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,21 @@
+defmodule RocketPay.Numbers do
+
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, string}) do
+    result =
+      string
+      |> String.split(",")
+      |> Enum.map(fn num -> String.to_integer(num) end)
+      |> Enum.sum()
+      |> Integer.to_string()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "inválid file."}}
+end
