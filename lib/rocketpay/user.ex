073f1848b8f42fcec84000ee2a687c646d72710b
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-18 19:17:58 -0300

    feat: news validates for user schema

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -12,11 +12,19 @@ defmodule RocketPay.User do
     field :email, :string
     field :password_hash, :string
     field :username, :string
+
+    timestamps()
   end
 
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+    |> validate_length(:username, min: 5)
   end
 end
