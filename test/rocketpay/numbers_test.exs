66631c9190b0bf973e0c3829ba118889ded677e3
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-18 15:24:07 -0300

    test: add numbers test

---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,22 @@
+defmodule RocketPay.NumbersTest do
+  use ExUnit.Case
+
+  alias RocketPay.Numbers
+
+
+  describe "sum_from_file/1" do
+    test "when filename is valid, returns the sum of values" do
+      response = Numbers.sum_from_file("numbers")
+      expected_response = {:ok, %{result: "38"}}
+
+      assert response == expected_response
+    end
+
+    test "when filename is inválid, returns error file" do
+      response = Numbers.sum_from_file("numberx")
+      expected_response = {:error, %{message: "inválid file."}}
+
+      assert response == expected_response
+    end
+  end
+end
